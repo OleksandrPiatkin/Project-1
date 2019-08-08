aa3c95d7e8c5075651134e29c9c3e737a6de224a
Alex <Oleksandr.Piatkin@gmx.com>
2019-08-08 13:45:10 +0200

    funktions for toDo list done

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-function doId() {
-        return Math.random() .toString(36) .substr(2, 16)
+/*jslint es6:true*/
+let list = document.querySelector('ul');
+list.addEventListener('click', function (ev) {
+        if(ev.target.tagName === 'LI') {
+                ev.target.classList.toggle('checked') 
+        } else if(ev.target.tagName === 'SPAN') {
+                let div = ev.target.parentNode;
+                div.remove();
         }
+} );
+
+let button = document.getElementById('toDo_button');
+button.addEventListener('click', newElement);
+
+function newElement() {
+        let li = document.createElement('li');
+        let inputValue = document.getElementById('toDo_input').value;
+        let text = document.createTextNode(inputValue);
+        li.appendChild(text);
+        if (inputValue == "") { 
+                } else {
+                document.getElementById('toDo_list').appendChild(li);
+        }
+        document.getElementById('toDo_input').value = "";
+        let span = document.createElement('SPAN');
+        let txt = document.createTextNode("X");
+        span.className = 'close';
+        span.appendChild(txt);
+        li.appendChild(span);
+}
